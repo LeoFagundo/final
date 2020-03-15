b555b3b2070eb9848ad0aca54f81f4b6c4de8d1e
LeoFagundo <leo.fagundo@kellogg.northwestern.edu>
2020-03-15 21:04:01 +0000

    coffee v0.2

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ reviews_table = DB.from(:reviews)
 users_table = DB.from(:users)
 
 before do
-    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+    @current_user = users_table.where(id: session["users_id"]).to_a[0]
 end
 
 # homepage and list of events (aka "index")
@@ -41,7 +41,7 @@ get "/coffee/:id" do
     pp @coffee
 
     @reviews = reviews_table.where(coffee_id: @coffee[:id]).to_a
-    # @avg_rating = reviews_table.where(coffee_id: @coffee[:id], going: true).count
+    @avg_rating = reviews_table.where(coffee_id: @coffee[:id]).avg(:rating)
 
     view "coffee_shop"
 end
@@ -75,43 +75,43 @@ end
 get "/reviews/:id/edit" do
     puts "params: #{params}"
 
-    @review = reviews_table.where(id: params["id"]).to_a[0]
-    @coffee = coffee_table.where(id: @review[:coffee_id]).to_a[0]
+    @reviews = reviews_table.where(id: params["id"]).to_a[0]
+    @coffee = coffee_table.where(id: @reviews[:coffee_id]).to_a[0]
     view "edit_review"
 end
 
-# # receive the submitted rsvp form (aka "update")
-# post "/rsvps/:id/update" do
-#     puts "params: #{params}"
+# receive the submitted reviews form (aka "update")
+post "/reviews/:id/update" do
+    puts "params: #{params}"
 
-#     # find the rsvp to update
-#     @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
-#     # find the rsvp's event
-#     @event = events_table.where(id: @rsvp[:event_id]).to_a[0]
+    # find the reviews to update
+    @reviews = reviews_table.where(id: params["id"]).to_a[0]
+    # find the review's coffee shop
+    @coffee = coffee_table.where(id: @reviews[:coffee_id]).to_a[0]
+    #in case user bug lets them edit another review
+    if @current_user && @current_user[:id] == @reviews[:id]
+        reviews_table.where(id: params["id"]).update(
+            rating: params["rating"],
+            comments: params["comments"]
+        )
 
-#     if @current_user && @current_user[:id] == @rsvp[:id]
-#         rsvps_table.where(id: params["id"]).update(
-#             going: params["going"],
-#             comments: params["comments"]
-#         )
+        redirect "/coffee/#{@coffee[:id]}"
+    else
+        view "error"
+    end
+end
 
-#         redirect "/events/#{@event[:id]}"
-#     else
-#         view "error"
-#     end
-# end
+# delete the reviews (aka "destroy")
+get "/reviews/:id/destroy" do
+    puts "params: #{params}"
 
-# # delete the rsvp (aka "destroy")
-# get "/rsvps/:id/destroy" do
-#     puts "params: #{params}"
+    @reviews = reviews_table.where(id: params["id"]).to_a[0]
+    @coffee = coffee_table.where(id: @reviews[:coffee_id]).to_a[0]
 
-#     rsvp = rsvps_table.where(id: params["id"]).to_a[0]
-#     @event = events_table.where(id: rsvp[:event_id]).to_a[0]
+    reviews_table.where(id: params["id"]).delete
 
-#     rsvps_table.where(id: params["id"]).delete
-
-#     redirect "/events/#{@event[:id]}"
-# end
+    redirect "/coffee/#{@coffee[:id]}"
+end
 
 # display the signup form (aka "new")
 get "/users/register" do
@@ -147,13 +147,13 @@ post "/logins/create" do
     puts "params: #{params}"
 
     # step 1: user with the params["email"] ?
-    @user = users_table.where(email: params["email"]).to_a[0]
+    @users = users_table.where(email: params["email"]).to_a[0]
 
-    if @user
+    if @users
         # step 2: if @user, does the encrypted password match?
-        if BCrypt::Password.new(@user[:password]) == params["password"]
+        if BCrypt::Password.new(@users[:password]) == params["password"]
             # set encrypted cookie for logged in user
-            session["user_id"] = @user[:id]
+            session["users_id"] = @users[:id]
             redirect "/"
         else
             view "register_failed"
@@ -166,6 +166,6 @@ end
 # logout user
 get "/logout" do
     # remove encrypted cookie for logged out user
-    session["user_id"] = nil
+    session["users_id"] = nil
     redirect "/logins/new"
 end
